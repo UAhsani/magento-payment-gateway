92a83f3736a3e99e040472da76043ceedb58c42c
aleksey.bulgakov <aleksey.bulgakov@avalab.io>
2021-04-13 16:43:37 +0500

    added js payment flow

--- view/frontend/web/js/view/payment/method-renderer/geidea-method.js
+++ view/frontend/web/js/view/payment/method-renderer/geidea-method.js
@@ -1,19 +1,23 @@
 define([
         'ko',
         'jquery',
+        'mage/translate',
         'Magento_Checkout/js/view/payment/default',
-        'Magento_Ui/js/modal/alert',
+        'Magento_Ui/js/model/messageList',
         'Magento_Checkout/js/model/payment/additional-validators',
         'Geidea_Payment/js/action/set-payment-method'
     ],
     function (
-        ko, $, Component, alert,
-        additionalValidators, setPaymentMethodAction) {
+        ko, $, $t,
+        Component, messageList, additionalValidators,
+        setPaymentMethodAction) {
         'use strict';
 
         return Component.extend({
             defaults: {
-                template: 'Geidea_Payment/payment/geidea'
+                template: 'Geidea_Payment/payment/geidea',
+                paymentActionError: $t('Something went wrong with your request. Please try again later.'),
+                processing: ko.observable(false)
             },
 
             getCode: function() {
@@ -24,39 +28,67 @@ define([
                 return true;
             },
 
-            setPaymentMethod: function () {
-                return setPaymentMethodAction(this.messageContainer);
+            setPaymentMethod: function (reject) {
+                var deferred = $.Deferred();
+
+                setPaymentMethodAction(this.messageContainer).done(function () {
+                    return deferred.resolve();
+                }).fail(function (response) {
+                    var error;
+
+                    try {
+                        error = JSON.parse(response.responseText);
+                    } catch (exception) {
+                        error = this.paymentActionError;
+                    }
+
+                    return reject(new Error(error));
+                }.bind(this));
+
+                return deferred.promise();
             },
 
-            reserve: function() {
+            reserve: function(reject) {
                 var params = {
                     'quote_id': window.checkoutConfig.quoteData['entity_id']
                 };
 
-                return $.post(window.checkoutConfig.payment.geidea_payment.reserveUrl, params)
+                this.processing(true);
+
+                var deferred = $.Deferred();
+
+                $.post(window.checkoutConfig.payment.geidea_payment.reserveUrl, params).done(function (data) {
+                    if (data.success)
+                        return deferred.resolve(data);
+        
+                    return reject(new Error(data['error_message']));
+                }).fail(function (jqXHR, textStatus, err) {
+                    return reject(err);
+                }.bind(this));
+
+                return deferred.promise();
             },
 
-            startPayment: function(data) {
+            startPayment: function(data, resolve, reject) {
                 
                 var onSuccess = function(_message, _statusCode) {
-                    console.log("suc");
+                    return resolve();
                 }
         
                 var onError = function(error) {
-                    console.log(error);
-                    console.log("error");
+                    return reject(new Error(error.responseMessage));
                 }
         
                 var onCancel = function() {
-                    console.log("can");
+                    return reject(new Error($t('Payment canceled')));
                 }
         
                 var api = new GeideaApi(this.clientConfig.merchantKey, onSuccess, onError, onCancel);
                 
                 api.configurePayment({
                     callbackUrl: "https://magento2.avalab.io/geidea/payment/callback/",// window.checkoutConfig.payment.geidea_payment.callbackUrl.replace("http", "https"),
-                    amount: 128,
-                    currency: "SAR"
+                    amount: data.amount,
+                    currency: "SAR"//data.currency
                 });
 
                 api.startPayment();
@@ -70,12 +102,23 @@ define([
                 }
 
                 if (this.validate() && additionalValidators.validate()) {
-                    this.setPaymentMethod()
-                        .then(this.reserve.bind(this))
-                        .then(this.startPayment.bind(this))
-                        .done(function () {
+                    
+                    $.Deferred(function (deferred) {
+                        this.setPaymentMethod.bind(this, deferred.reject)()
+                            .then(this.reserve.bind(this, deferred.reject))
+                            .then(function(data) {
+                                return this.startPayment.bind(this, data, deferred.resolve, deferred.reject)();
+                            }.bind(this));
+                    }.bind(this))
+                        .promise()
+                        .done(function() {
+                            this.processing(false);
                             console.log("done xd");
-                        });
+                        }.bind(this))
+                        .fail(function(err) {
+                            this.processing(false);
+                            this.addError(err.message);
+                        }.bind(this));
 
                     return false;
                 }
@@ -83,20 +126,9 @@ define([
                 return false;
             },
 
-            addError: function (message, type) {
-                type = type || 'error';
-                customerData.set('messages', {
-                    messages: [{
-                        type: type,
-                        text: message
-                    }],
-                    'data_id': Math.floor(Date.now() / 1000)
-                });
-            },
-    
-            addAlert: function (message) {
-                alert({
-                    content: message
+            addError: function (message) {
+                messageList.addErrorMessage({
+                    message: message
                 });
             }
         });
