98727b08115af93fcd8cd84a0974c39ca017a3d8
aleksey.bulgakov <aleksey.bulgakov@avalab.io>
2021-04-20 12:53:21 +0500

    min max order validation

---
+++ Gateway/Validator/AvailabilityValidator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Geidea\Payment\Gateway\Validator;
+
+use Magento\Payment\Gateway\Validator\AbstractValidator;
+use Magento\Payment\Gateway\Validator\ResultInterfaceFactory;
+
+use Geidea\Payment\Gateway\Config\Config;
+
+class AvailabilityValidator extends AbstractValidator
+{
+    private $config;
+
+    public function __construct(
+        ResultInterfaceFactory $resultFactory,
+        Config $config
+    ) {
+        $this->config = $config;
+        parent::__construct($resultFactory);
+    }
+    
+    public function validate(array $validationSubject)
+    {
+        $quote = $validationSubject['quote'];
+
+        if ($quote === null)
+            return $this->createResult(false);
+        
+        $storeId = $quote->getStoreId();
+        $amount = $quote->getBaseGrandTotal();
+
+        $minOrderTotal = $this->config->getValue('minOrderTotal', $storeId);
+        if (!empty($minOrderTotal) && $amount < $minOrderTotal)
+            return $this->createResult(false);
+        
+        $maxOrderTotal = $this->config->getValue('maxOrderTotal', $storeId);
+        if (!empty($maxOrderTotal) && $amount > $maxOrderTotal)
+            return $this->createResult(false);
+
+        return $this->createResult(true);
+    }
+}
