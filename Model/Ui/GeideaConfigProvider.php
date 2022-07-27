1dec144e868c3168ff87cd090a479d51de0da0d8
aleksey.bulgakov <aleksey.bulgakov@avalab.io>
2022-07-27 14:09:44 +0500

    add `receipt_enabled` field

--- Model/Ui/GeideaConfigProvider.php
+++ Model/Ui/GeideaConfigProvider.php
@@ -4,6 +4,7 @@ namespace Geidea\Payment\Model\Ui;
 use Magento\Checkout\Model\ConfigProviderInterface;
 use Magento\Framework\Locale\ResolverInterface;
 use Magento\Framework\Session\SessionManagerInterface;
+use Magento\Framework\Stdlib\BooleanUtils;
 use Magento\Framework\UrlInterface;
 use Magento\Payment\Gateway\ConfigInterface;
 
@@ -17,17 +18,20 @@ class GeideaConfigProvider implements ConfigProviderInterface
     private $session;
     private $urlBuilder;
     private $localeResolver;
+    private $booleanUtils;
 
     public function __construct(
         ConfigInterface $config,
         SessionManagerInterface $session,
         UrlInterface $urlBuilder,
-        ResolverInterface $localeResolver
+        ResolverInterface $localeResolver,
+        BooleanUtils $booleanUtils
     ) {
         $this->config = $config;
         $this->session = $session;
         $this->urlBuilder = $urlBuilder;
         $this->localeResolver = $localeResolver;
+        $this->booleanUtils = $booleanUtils;
     }
 
     public function getConfig()
@@ -45,7 +49,8 @@ class GeideaConfigProvider implements ConfigProviderInterface
                         'reserveUrl' => $this->urlBuilder->getUrl($this->config->getValue("reserveUrl", $storeId)),
                         'authorizeUrl' => $this->urlBuilder->getUrl($this->config->getValue("authorizeUrl", $storeId)),
                         'callbackUrl' => $this->urlBuilder->getUrl($this->config->getValue("callbackUrl", $storeId)),
-                        'language' => $this->localeResolver->getLocale() == 'ar_SA' ? 'ar' : 'en'
+                        'language' => $this->localeResolver->getLocale() == 'ar_SA' ? 'ar' : 'en',
+                        'receiptEnabled' => $this->booleanUtils->toBoolean($this->config->getValue("receiptEnabled", $storeId))
                     ],
                     'vaultCode' => self::VAULT_CODE
                 ]
--- view/frontend/web/js/view/payment/method-renderer/geidea-method.js
+++ view/frontend/web/js/view/payment/method-renderer/geidea-method.js
@@ -115,6 +115,7 @@ define([
                     currency: data.currency,
                     merchantReferenceId: data.orderId,
                     merchantLogoUrl: this.clientConfig.logoUrl,
+                    isTransactionReceiptEnabled: this.clientConfig.receiptEnabled,
                     language: this.clientConfig.language,
                     paymentOperation: "PreAuthorize",
                     cardOnFile: this.isVaultEnabled() && this.vaultEnabler.isActivePaymentTokenEnabler(),
